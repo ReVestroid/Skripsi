0d5aecdc682a1a7d98cf0841af504674410b4bcd
ReVestroid <115411923+ReVestroid@users.noreply.github.com>
2024-06-20 20:54:20 +0700

    Update Beranda.py

--- Beranda.py
+++ Beranda.py
@@ -4,7 +4,49 @@ import subprocess
 
 st.set_page_config(page_title="Aplikasi K-Medoids")
 st.title("Aplikasi Steam Menentukan Penjualan Game Terbaik K-Medoids")
-st.sidebar.title('Sidebar Example')
+
+def main():
+    st.sidebar.title('Sidebar Options')
+    st.sidebar.header('Choose an option')
+
+    option = st.sidebar.selectbox(
+        'Select an option:',
+        ('Option 1', 'Option 2', 'Option 3', 'Option 4')
+    )
+
+    if option == 'Option 1':
+        st.title('Option 1 selected')
+        st.write('You selected Option 1.')
+        # Menjalankan file coba.py
+        try:
+            subprocess.run(['python', 'Menu_5_Chart.py'])
+        except Exception as e:
+            st.error(f"Error running coba.py: {e}")
+    elif option == 'Option 2':
+        st.title('Option 2 selected')
+        st.write('You selected Option 2.')
+        # Menjalankan file coba.py
+        try:
+            subprocess.run(['python', 'Menu_6_Clustering.py'])
+        except Exception as e:
+            st.error(f"Error running coba.py: {e}")
+    elif option == 'Option 3':
+        st.title('Option 3 selected')
+        st.write('You selected Option 3.')
+        # Menjalankan file coba.py
+        try:
+            subprocess.run(['python', 'Menu_7_Price.py'])
+        except Exception as e:
+            st.error(f"Error running coba.py: {e}")
+    elif option == 'Option 4':
+        st.title('Option 3 selected')
+        st.write('You selected Option 3.')
+        # Menjalankan file coba.py
+        try:
+            subprocess.run(['python', 'Menu_8_Review.py'])
+        except Exception as e:
+            st.error(f"Error running coba.py: {e}")
+
 # Function to execute the external script
 def run_external_script():
     subprocess.Popen(["C:/Program Files/Python312/python.exe", "e:/CRUD/Steam_Combine.py"])
